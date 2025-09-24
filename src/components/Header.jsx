91c9418ade208e42d0277813362a0c1b55399139
Kumail-Devminds <kumaildevminds@gmail.com>
2025-09-25 02:26:16 +0500

    Adjust Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,18 +8,18 @@ const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
         <div className="max-w-screen-xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-3">
-            <h1 className="text-3xl font-bold flex items-center text-zinc-50">
-              <a href="/" className="logo">
+            <h1 className="">
+              <a href="/" className="logo text-3xl font-bold flex items-center text-zinc-50">
                 <img
                   src="/images/logo.svg"
                   width={40}
                   height={40}
                   alt="Hafiz Muhammad Kumail"
                 />
+                <span>
+                  Kumail
+                </span>
               </a>
-              <span>
-                Kumail
-              </span>
             </h1>
         
             <div className="relative md:justify-self-center">
